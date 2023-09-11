d13af173449a61fea0971e0f2a3bd197f4c5c002
Arthur Cosme <cosmearthur14@gmail.com>
2023-09-11 15:37:32 -0300

    Customizando o tema do Angular Material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'list'},
+  {
+    path: 'list',
+    loadChildren: () => import('./list/list.module').then(m => m.ListModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
